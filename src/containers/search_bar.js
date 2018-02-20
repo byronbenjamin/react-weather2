fd4b9ab5db45121e39ebc8630b0f0a3f48de3c3d
Byron Gage <byronbenjamin.chi@gmail.com>
2018-02-19 19:43:13 -0600

    initial commit

---
+++ src/containers/search_bar.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+
+class SearchBar extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      term: ''
+    }
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({
+      term: event.target.value
+    })
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    //fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} action="" className="input-group">
+        <input
+          type="text"
+          placeholder="Get a five day forecast for your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// pass null to make sure mapDispatchToProps is the second argument since theres no state.
+
+export default connect(null, mapDispatchToProps)(SearchBar);
